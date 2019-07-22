42a322844b75e6f06a575c281a971a7d8d5baa22
Jiangyu Li <17302010048@fudan.edu.cn>
2019-07-22 15:36:50 +0800

    merge出了bug，目前大部分已经找回

--- firstweb/src/dao/impl/ExhibitDaoImpl.java
+++ firstweb/src/dao/impl/ExhibitDaoImpl.java
@@ -12,7 +12,7 @@ public class ExhibitDaoImpl extends DAO<Exhibit> implements ExhibitDao {
 
     @Override
     public Exhibit getExhibit(int id) {
-        String sql = "SELECT iditems id,itemname name,year,place,age,detail,hotdegree hotDegree,pic FROM exhibits WHERE iditems = ?";
+        String sql = "SELECT iditems id,itemname name,year,place,age,detail,hotdegree hotDegree,pic FROM exhibit WHERE iditems = ?";
         return get(sql,id);
     }
 
@@ -27,7 +27,7 @@ public class ExhibitDaoImpl extends DAO<Exhibit> implements ExhibitDao {
     }
 
     private List<Exhibit> getList(int number, String orderType){
-        String sql = "select iditems id,itemname name,year,place,age,detail,hotdegree hotDegree,pic from exhibits order by " + orderType + " desc";
+        String sql = "select iditems id,itemname name,year,place,age,detail,hotdegree hotDegree,pic from exhibit order by " + orderType + " desc";
         List<Exhibit> resultSet = getForList(sql);
         List<Exhibit> returnSet = new ArrayList<>();
         if(number <= resultSet.size()){
@@ -40,16 +40,30 @@ public class ExhibitDaoImpl extends DAO<Exhibit> implements ExhibitDao {
 
     @Override
     public void updateHotDegree(int id) {
-        String sql = "update museum_pj.exhibits set hotdegree = hotdegree + 1 where iditems = ?";
+        String sql = "update museum_pj.exhibit set hotdegree = hotdegree + 1 where iditems = ?";
         update(sql,id);
     }
 
     @Override
     public List<Exhibit> searchExhibits(String[] searchItems) {
-        String sql = "select iditems id,itemname name,year,place,age,detail,hotdegree hotDegree,pic from exhibits " +
+        String sql = "select iditems id,itemname name,year,place,age,detail,hotdegree hotDegree,pic from exhibit " +
                      "where itemname like ? and detail like ? and place like ? " +
                      "order by hotdegree desc";
         return getForList(sql,"%" + searchItems[0] + "%","%" + searchItems[1] + "%","%" + searchItems[2] + "%");
     }
 
+    public void updateExhibit(int id,String name,String detail,String place,String age,String year,String pic){
+        String sql = "update museum_pj.exhibit set itemname=? , detail=? , place=? , age=? , year=? , pic=? where iditems=?";
+        update(sql,name,detail,place,age,year,pic,id);
+    }
+
+    public void addExhibit(String name,String detail,String place,String age,String year,String pic){
+        String sql = "insert into museum_pj.exhibit (itemname,detail,place,age,year,pic,hotdegree) values (?,?,?,?,?,?,?)";
+        update(sql,name,detail,place,age,year,pic,0);
+    }
+
+    public void deleteExhibit(int id){
+        String sql = "delete from museum_pj.exhibit where iditems=?";
+        update(sql,id);
+    }
 }
