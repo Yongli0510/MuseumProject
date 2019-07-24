73d591b31e24d6693b67c2645d82e0430ee8d114
Jiangyu Li <17302010048@fudan.edu.cn>
2019-07-24 15:29:23 +0800

    验证码已修改

--- firstweb/src/service/MailService.java
+++ firstweb/src/service/MailService.java
@@ -1,7 +1,19 @@
 package service;
 
+import com.sun.nio.sctp.MessageInfo;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
 public class MailService {
 
@@ -36,4 +48,37 @@ public class MailService {
             ex.printStackTrace();
         }
     }
+
+    public void sslSend(String mail)
+            throws AddressException, MessagingException, IOException {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.163.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("keyhe0@163.com","jjccaow21");
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("keyhe0@163.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(mail));
+            message.setSubject("这是来自官方的验证邮件，请勿回复");
+            message.setText("验证码："+getRandomNumber());
+
+            Transport.send(message);
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
